0210bd4efacb07b1ea74e9e2bfb5dda3e3e6a032
Mudit Bothra <muditbothra@Mudits-MacBook-Pro.local>
2020-06-06 17:59:31 +0530

    Minor Changes

--- backend/server.py
+++ backend/server.py
@@ -19,7 +19,7 @@ print('new Flask!')
 app.debug=True
 
 if __name__ == '__main__':
-    app.run(host='127.0.0.1')
+    app.run(host='127.0.0.1', debug=True)
 
 @app.route("/add_donor/",  methods=['POST'])
 def add_donor():
